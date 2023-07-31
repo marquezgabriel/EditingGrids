7cadfaba6d1ce1f5f510d5cf495e6e756a889f7a
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-07-31 09:18:18 -0400

    Initial Commit

---
+++ EditingGrids/EditingGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditingGridsApp.swift
+//  EditingGrids
+//
+//  Created by Gabriel Marquez on 2023-07-31.
+//
+
+import SwiftUI
+
+@main
+struct EditingGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
